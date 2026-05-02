e25f40862c245f7f7220821439eb4a31551b8cff
khang2005 <Khangtran082005@gmail.com>
2026-05-02 03:12:04 -0700

    Add worker route and deploy step to CI/CD

--- cloudflare.tf
+++ cloudflare.tf
@@ -27,16 +27,29 @@ variable "domain" {
   type        = string
 }
 
-resource "cloudflare_record" "this" {
-  zone_id = var.zone_id
-  name    = var.domain
-  value   = var.origin_ip
-  type    = "A"
-  ttl     = 1
-  proxied = true
+variable "worker_name" {
+  description = "Cloudflare Worker name"
+  type        = string
+  default     = "docs-worker"
 }
 
 variable "origin_ip" {
   description = "Origin server IP"
   type        = string
+}
+
+# A record for root domain (uncomment when you have a real origin IP)
+# resource "cloudflare_record" "this" {
+#   zone_id = var.zone_id
+#   name    = var.domain
+#   content = var.origin_ip
+#   type    = "A"
+#   ttl     = 1
+#   proxied = true
+# }
+
+resource "cloudflare_worker_route" "docs" {
+  zone_id     = var.zone_id
+  pattern     = "docs.khangduytran.xyz/*"
+  script_name = var.worker_name
 }
